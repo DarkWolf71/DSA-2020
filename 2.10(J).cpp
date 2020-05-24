b40b6526b29065cfd6a72c309522cd5c773c1117
DarkWolf71 <65805212+DarkWolf71@users.noreply.github.com>
2020-05-24 12:27:09 +0530

    not working

--- 2.10(J).cpp
+++ 2.10(J).cpp
@@ -8,30 +8,30 @@
 using namespace std;
 
 bool check( long int value){
-    long int n = sqrt(value), b=0;
-    if(n>2){
-        for(long int i=2;i<=n;i++){
-            if(value%i == 0)
-                b++;
-        }
-    }
-    if(b == 0)
-        return true;
-    else
-        return false;
+	long int n = sqrt(value), b=0;
+	if(n>=2){
+		for(long int i=2;i<=n;i++){
+			if(value%i == 0)
+				return false;
+		}
+	}
+	if(b == 0)
+		return true;
+	else
+		return false;
 }
 
 int main(){
-    long int a,b=0,c=0,temp;
-    cin>>a;
-    for (long int i=2; i<=a; i++){
-        if((a%i==0) && (check(i))){
-            b++;
-            c=c^i;
-        }
-    }
-    if(b==1)
-        c=a;
-    cout<<c;
-    return 0;
+	long int a,b=0,c=0,temp;
+	cin>>a;
+	for (long int i=2; i<=a; i++){
+		if((a%i==0) && (check(i))){
+		b++;
+		c=c^i;
+		}
+	}
+	if(b==1)
+		c=a;
+	cout<<c;
+	return 0;
 }
